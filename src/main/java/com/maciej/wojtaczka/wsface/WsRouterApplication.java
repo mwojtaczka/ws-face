a7daaf943694bff6c09ea69122cc7f43724dc03f
mwojtacz <mwojtacz@akamai.com>
2022-02-17 10:03:44 +0100

    Init

---
+++ src/main/java/com/maciej/wojtaczka/wsface/WsRouterApplication.java
@@ -0,0 +1,13 @@
+package com.maciej.wojtaczka.wsface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsRouterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsRouterApplication.class, args);
+    }
+
+}
